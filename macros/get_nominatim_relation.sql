5fb3317373cc151a6e11287bd865f9e011b07cc1
01001100 <davidwhittingham94@gmail.com>
2024-10-28 12:05:05 +0100

    Remove unused macro.

--- macros/get_nominatim_relation.sql
+++
@@ -1,6 +0,0 @@
-{% macro get_nominatim_relation(osm_id) %}
-    {% set url = "https://nominatim.openstreetmap.org/details?osmtype=R&osmid=" ~ osm_id ~ "&format=json" %}
-    {% set query = "SELECT * FROM read_json_objects('" ~ url ~ "')" %}
-    {% set response = run_query(query) %}
-    {{ return(response) }}
-{% endmacro %}
